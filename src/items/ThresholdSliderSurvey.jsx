04f6aebe416766962787b7702cd4b9740548e7d3
Elvictor01 <vikingdavid41@gmail.com>
2021-04-01 15:16:29 +0100

    Update 002

--- src/items/ThresholdSliderSurvey.jsx
+++ src/items/ThresholdSliderSurvey.jsx
@@ -12,4 +12,64 @@ import {
 class ThresholdCurve extends Component {
   constructor(props) {
     super(props);
-    
+
+    this.state = this.initialState;
+    this.resetState = this.resetState.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.ThresholdCurve = this.ThresholdCurve.bind(this);
+  }
+
+
+  // setting the initial state of this component 
+  get initialState() {
+    return {
+      // this submitted variable is required 
+      submitted: false,
+      // ###TODO###: add more variables below 
+    };
+  }
+
+  resetState() {
+    this.setState(this.initialState);
+  }
+
+  componentDidMount() {
+    const { childRef } = this.props;
+    childRef(this);
+    this.getData();
+    this.props.setWhichItem("threshold-slider");
+  }
+
+  componentWillUnmount() {
+    const { childRef } = this.props;
+    childRef(undefined);
+  }
+
+  getData() {
+    const db = this.props.expt.dbInfo.db;
+    const studyName = this.props.expt.dbInfo.col.split("-")[0];
+    const exptName = this.props.expt.dbInfo.col.split("-")[1];
+    this.props.getExpt(db, studyName, exptName);
+  }
+ 
+
+}
+
+
+
+
+ThresholdSliderSurvey.propTypes = {
+  getExpt: PropTypes.func.isRequired,
+  expt: PropTypes.object.isRequired,
+  storeAnswer: PropTypes.func.isRequired
+}
+
+const mapStateToProps = state => ({
+  expt: state.expt
+})
+
+export default connect(
+  mapStateToProps,
+  { getExpt, storeAnswer }
+)(ThresholdSliderSurvey);
