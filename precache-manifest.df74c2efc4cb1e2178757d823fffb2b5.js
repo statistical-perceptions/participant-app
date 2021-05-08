ca024348d0b7c001143da8b6f9c96fae64d088ac
Elvictor01 <vikingdavid41@gmail.com>
2021-05-08 05:22:19 +0100

    Updates

--- precache-manifest.df74c2efc4cb1e2178757d823fffb2b5.js
+++ precache-manifest.df74c2efc4cb1e2178757d823fffb2b5.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d0856a70dad16187c870e26f212716d8",
+    "revision": "8b5897a220a5a09ea870eeb38bdf5854",
     "url": "/participant-app/index.html"
   },
   {
-    "revision": "b3ba71b9a709b51ea802",
+    "revision": "facbe01f404779235db9",
     "url": "/participant-app/static/css/main.9b6a10fb.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/participant-app/static/js/2.092cfaba.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b3ba71b9a709b51ea802",
-    "url": "/participant-app/static/js/main.9e2c3d7e.chunk.js"
+    "revision": "facbe01f404779235db9",
+    "url": "/participant-app/static/js/main.096cddeb.chunk.js"
   },
   {
     "revision": "99ffa7176f9e50ac4925",
